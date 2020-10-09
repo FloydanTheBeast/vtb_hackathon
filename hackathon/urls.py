b8fde612441be1cc62184f19d80976a9056ee08c
artgas1 <gasparyanartem2002@gmail.com>
2020-10-09 16:42:37 +0300

    added registration/login

--- hackathon/urls.py
+++ hackathon/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+api_urlpatterns = [
+    path('', include('rest_framework.urls')),
+    path('accounts/', include('rest_registration.api.urls')),
+]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('rest_framework.urls'))
+    path('api/', include(api_urlpatterns))
 ]
