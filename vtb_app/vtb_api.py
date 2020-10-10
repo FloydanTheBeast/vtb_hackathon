da9a9d2d15ec0e3eb92d9219183d9e1e5e33a90b
Shadi <floydanthebeast@gmail.com>
2020-10-10 13:19:05 +0300

    Fix requirements.txt

--- vtb_app/vtb_api.py
+++ vtb_app/vtb_api.py
@@ -10,11 +10,12 @@ def car_recognize_method(image):
     image = base64.b64encode(image).decode()
     headers = {"accept": "application/json", "content-type": "application/json", "x-ibm-client-id": key}
     data = {"content": image}
-    r = requests.post(url, data=json.dumps(data), headers=headers)
+    r = requests.post(url, data = json.dumps(data), headers = headers)
+
     if r.status_code == 200:
         cars = json.loads(r.text).get("probabilities")
         cars = list(cars.items())
-        cars.sort(key=lambda i: i[1], reverse=True)
+        cars.sort(key = lambda i: i[1], reverse = True)
         return cars[0]
     else:
         return r.text
